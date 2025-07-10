8d44e5aa8dd4ad00d8c557b0157c78589dd2bd4d
ItosEO <m2265514616@163.com>
2025-07-11 00:36:53 +0800

    更新版本号至0.31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 35
         targetSdk = 36
         versionCode = 20250710
-        versionName = "0.22"
+        versionName = "0.31"
 
         ndk {
             // 设置支持的SO库架构
